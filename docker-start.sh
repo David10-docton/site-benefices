3c5536e6a2b3071e9a0db06322de1f02ed766c29
David10-docton <deogracias.hinvi10@gmail.com>
2026-06-29 10:47:54 +0100

    correction du dashboad

--- docker-start.sh
+++ docker-start.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 cd /var/www/html
+
+# Permissions storage
+chmod -R 777 /var/www/html/storage
+chmod -R 777 /var/www/html/bootstrap/cache
+
+# Migrations
 php artisan migrate --force
+
+# Vider les caches
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
+
+# Reconstruire les caches
 php artisan config:cache
 php artisan route:cache
-php artisan view:cache
+
+# Démarrer Apache
 apache2-foreground
